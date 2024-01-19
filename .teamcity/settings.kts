a8757ad9ef871b0584425d3a49e353371b73d003
Eugene Kuznetsov <eugene.kuznetsov@skyboxlabs.com>
2024-01-19 14:48:30 -0800

    resolve UI

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -45,7 +45,7 @@ object Build : BuildType({
         maven {
             name = "Basic Build"
             id = "Maven2"
-            goals = "clean test"
+            goals = "clean package"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
