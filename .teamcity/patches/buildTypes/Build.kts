2c12ef8abfed60042ffa0a84d61974ab24f647a7
ekuznets <ekuznets@TeamCity>
2024-01-30 17:07:08 -0800

    TeamCity change in 'Eugenelist' project: general settings of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -11,6 +11,11 @@ To apply the patch, change the buildType with id = 'Build'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Build")) {
+    check(artifactRules == "") {
+        "Unexpected option value: artifactRules = $artifactRules"
+    }
+    artifactRules = "*.txt"
+
     expectSteps {
         maven {
             goals = "clean compile"
